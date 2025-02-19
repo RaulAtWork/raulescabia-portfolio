bcdd062476f3a3e4c4875f9a931cd7d264525072
Raul Escabia <raulescabia.work@gmail.com>
2025-02-19 12:14:16 +0100

    introduction

--- src/pages/sections/Introduction.jsx
+++ src/pages/sections/Introduction.jsx
@@ -1,12 +1,32 @@
+import { faLinkedin, faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 function Introduction() {
   return (
-    <section>
-    <img src="https://placehold.co/200x200"/>
-      <h1>{"<h1> "}Raul Escabia{" </h1>"}</h1>
-      <p>{"<p> "}Senior Full-Stack web developer who specializes in Front-End.{" </p>"}</p>
-      <a>Linked in</a> 
-      <a>Github</a>
-      <button>Download CV</button>
+    <section className="intro">
+      <img src="https://placehold.co/200x200" />
+      <div className="intro-container">
+        <h1 className="intro-name">
+          <span className="text-secondary">{"<h1> "}</span>Raúl Escabia
+          <span className="text-secondary">{" </h1>"}</span>
+        </h1>
+        <p>
+          <span className="text-secondary">{"<p> "}</span>
+          <b className="text-primary-color">Senior Full-Stack </b>
+          web developer who{" "}
+          <b className="text-accent-color">specializes in Front-End</b>
+          <span className="text-secondary">{" </p>"}</span>
+        </p>
+        <div className="intro-links">
+          <a>
+            <FontAwesomeIcon className="icon-l" icon={faLinkedin} />
+          </a>
+          <a>
+            <FontAwesomeIcon className="icon-l" icon={faSquareGithub} />
+          </a>
+          <button className="button-secondary">Download CV</button>
+        </div>
+      </div>
     </section>
   );
 }
